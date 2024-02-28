714bf5af1f146f37e1c885cbef0231204d9219b4
laleliloluii <147074461+laleliloluii@users.noreply.github.com>
2024-02-28 23:26:43 +0800

    random updates

--- app/src/main/java/com/example/flamespace/MainActivity.kt
+++ app/src/main/java/com/example/flamespace/MainActivity.kt
@@ -1,29 +1,25 @@
 package com.example.flamespace
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.example.flamespace.R.id.mainLogin
-import com.example.flamespace.R.id.mainSignup
 
 class MainActivity : AppCompatActivity() {
 
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val buttonClick = findViewById<Button>(mainLogin)
+        val buttonClick = findViewById<Button>(R.id.mainLogin)
         buttonClick.setOnClickListener {
-            val intent = Intent(this, SignIn::class.java)
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
 
-        val btn = findViewById<Button>(mainSignup)
+        val btn = findViewById<Button>(R.id.mainSignup)
         btn.setOnClickListener {
-            val intent = Intent(this, Sign_up::class.java)
+            val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
     }
---
+++ app/src/main/java/com/example/flamespace/Students.kt
@@ -0,0 +1,11 @@
+package com.example.flamespace
+
+data class Students(
+    val userId: Int,
+    val id: String,
+    val firstName: String,
+    val middleName: String,
+    val lastName: String,
+    val studentId: Int,
+    val phinmaEmail: String,
+)
